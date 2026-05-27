b083e534eed3ba40e758fda174b60cbd5f92f6bf
Bhuvaneswaran-s <bhuvaneswaran80567@gmail.com>
2026-05-27 16:11:58 +0530

    Add files via upload
    DAY3

---
+++ array3.java
@@ -0,0 +1,12 @@
+public class array3{
+    public static void main(String[] args) {
+        int[] numbers = {76, 83, 55, 36};
+        int max = numbers[0]; 
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i]; 
+            }
+        }
+        System.out.println(max);
+    }
+}
